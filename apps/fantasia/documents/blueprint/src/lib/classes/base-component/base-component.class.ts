22146922820154dfa50b18a64b86cfbe4c5bc369
Andre Davcev <>
2020-03-15 21:58:14 -0400

    npm run update (angular 9)

--- apps/fantasia/documents/blueprint/src/lib/classes/base-component/base-component.class.ts
+++ apps/fantasia/documents/blueprint/src/lib/classes/base-component/base-component.class.ts
@@ -1,9 +1,10 @@
-import { OnDestroy, HostListener } from '@angular/core';
+import { OnDestroy, HostListener, Directive } from '@angular/core';
 import { Subject } from 'rxjs';
 
 /**
  * Handles component destroy to clear memory leaks
  */
+@Directive()
 export class BaseComponent implements OnDestroy {
 
   public destroy$: Subject<boolean> = new Subject<boolean>();
--- apps/fantasia/jest.config.js
+++ apps/fantasia/jest.config.js
@@ -1,9 +1,10 @@
 module.exports = {
-  name: "fantasia",
-  preset: "../../jest.config.js",
-  coverageDirectory: "../../coverage/apps/fantasia/",
+  name: 'fantasia',
+  preset: '../../jest.config.js',
+  coverageDirectory: '../../coverage/apps/fantasia/',
   snapshotSerializers: [
-    "jest-preset-angular/AngularSnapshotSerializer.js",
-    "jest-preset-angular/HTMLCommentSerializer.js"
+    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
+    'jest-preset-angular/build/AngularSnapshotSerializer.js',
+    'jest-preset-angular/build/HTMLCommentSerializer.js'
   ]
 };
