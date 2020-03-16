520d087ca901ea05cfcb5f578134b5cd4e972574
Andre Davcev <>
2020-03-16 00:48:48 -0400

    Change blog link

--- apps/fantasia/src/app/constants/app-list.const.ts
+++ apps/fantasia/src/app/constants/app-list.const.ts
@@ -2,7 +2,7 @@ import { AppProperties } from '../models';
 import { App } from '../enums';
 
 export const AppList: Array<AppProperties> = [
-  { key: App.Blog, link: 'https://medium.com/fantasia' },
+  { key: App.Blog, link: 'https://medium.com/@andre.davcev' },
   { key: App.Resume },
   { key: App.Firefly, link: 'https://firefly.im' },
   { key: App.Starcraft },
